af63e21480cb318852799f8e37f881e09c3bd2af
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-03-28 01:15:53 -0400

    Refactor & Tweak
    - Add some comments
    - Change some file names
    - Improve variable naming
    - etc.

--- CCA-Java/src/kiosk/Main.java
+++ CCA-Java/src/kiosk/Main.java
@@ -1,41 +1,38 @@
 package kiosk;
 
+/* FXML Imports */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.*;
+/* Other Imports */
 import java.util.ArrayList;
 
 public class Main extends Application {
+    private static Scene splashScreen, homeScreen;
+    public static final double WIDTH = 600, HEIGHT = 800;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        splashScreen = new Scene(FXMLLoader.load(getClass().getResource("splash.fxml")), WIDTH, HEIGHT);
+        homeScreen = new Scene(FXMLLoader.load(getClass().getResource("home.fxml")), WIDTH, HEIGHT);
+
+        primaryStage.setTitle("Ordering Kiosk");
+        primaryStage.setScene(splashScreen);
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        // launch(args);
-        Database DB = new Database("jdbc:sqlite:src\\menu.db");
+        /* Open Window */
+        launch(args);
+
+        /* ↓↓ Testing the DB Functions ↓↓ */
+        Database DB = new Database("jdbc:sqlite:src\\master.db");
 
         ArrayList<String> types = Menu.generateTypes(DB);
-        Order.addToOrder(DB, "greasysticks", 3.69F);
-        // need to delete the orders table when the customer is finished their transaction / when program ends
+        Order.addToOrder(DB, "greasySticks", 3.69F);
+
         DB.closeConnection();
-        /*for (String t : types) {
-            ArrayList<String> items = itemsFromType(statement, t);
-            System.out.println("Type: " + t);
-            for (String x : items){
-                System.out.println(x);
-            }
-            System.out.println("");
-        }
-        closeConnection(statement, conn);*/
     }
 }
