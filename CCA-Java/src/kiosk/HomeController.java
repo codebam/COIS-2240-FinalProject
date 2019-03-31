46e901273f54f6e987a13289eb6d6f2413270530
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-03-30 23:45:48 -0400

    Make a lot of tweaks to last commit (merge)
    - Turn the main database object into a static field of Main, to be referenced
      whenever.
    - Move table generating function to it's own controller, to be dealt with later
      - Create a .fxml file for it, too
    - Implement Initializable in the controllers to allow for a cleaner start()
      method
    - Move FXMLLoader.load() methods to after the sideBar generations
    - Take the testing out of the main() method
    - Implement loading the different types of food into the home screen
      - Looks like crap right now, tidy it up once the blocks are developed

--- CCA-Java/src/kiosk/HomeController.java
+++ CCA-Java/src/kiosk/HomeController.java
@@ -1,25 +1,34 @@
 package kiosk;
 
+import java.net.URL;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.fxml.FXML;
-
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
-    /* Dimensions for VBox */
-    public static final int PREV_HEIGHT = 170; //Preview Height
-    public static final int WIDTH = Main.HALF_WIDTH; //Width of the Side-Panel
-    public static final int HEIGHT = Main.HEIGHT - PREV_HEIGHT; //Height of the VBox
+    public static final int BANNER_HEIGHT = Main.QUARTER_HEIGHT;
+    public static final int OBJECT_HEIGHT = (int)((Main.HEIGHT - BANNER_HEIGHT) * 0.85);
+    public static final int OBJECT_SIDE_OFFSET = (int)(Main.WIDTH * 0.065);
+    public static final int OBJECT_TOP_POS = BANNER_HEIGHT + OBJECT_SIDE_OFFSET;
 
     @FXML AnchorPane root;
-    static public VBox dick;
+    @FXML GridPane typesGrid;
     static AnchorPane sideBar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        root.getChildren().add(dick);
+        ArrayList<String> types = Menu.generateTypes();
+        int i = 0;
+        int j = 0;
+        for (String t : types) {
+            Pane p = new Pane(new Button(t));
+            typesGrid.add(p, i, j);
+            i = ++i % 2;
+            j = ++j % 3;
+        }
     }
 
     @FXML
